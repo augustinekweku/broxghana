2b0fc319c442eb6a8f42fa280fae13ccee0f593f
Augustine Nani <augustine.nani@hubtel.com>
2023-08-15 15:53:46 +0000

    images optimization

---
+++ app/utils/constants.ts
@@ -0,0 +1,27 @@
+export const OUR_WORKS = [
+  {
+    title: "Restaurant POS System",
+    image: "/assets/images/img1.png",
+    description:
+      "Brox Restaurant POS System is a software that helps restaurant owners to manage their business effectively. It is a user friendly software that can be used by anyone with little or no knowledge in IT. It is a cloud based software that can be accessed anywhere and anytime. It is a software that can be used by any restaurant owner to manage their business effectively. It is a user friendly software that can be used by anyone with little or no knowledge in IT. It is a cloud based software that can be accessed anywhere and anytime.",
+    detailsPage: "/restaurant-pos-system",
+  },
+  {
+    title: "WASSCE / GES Result Checker",
+    image: "/assets/images/img2.png",
+    description: "",
+    detailsPage: "/wassce-ges-result-checker",
+  },
+  {
+    title: "School Management System",
+    image: "/assets/images/img3.png",
+    description: "",
+    detailsPage: "/school-management-system",
+  },
+  {
+    title: "Point of Sale Software(POS)",
+    image: "/assets/images/img4.png",
+    description: "",
+    detailsPage: "/point-of-sale-software",
+  },
+];
